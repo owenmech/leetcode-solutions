9a5e48a7364ebef214fc906f5885e3023df3b6bb
owenmech <31045445+owenmech@users.noreply.github.com>
2025-11-07 10:39:13 -0800

    1488

---
+++ src/1488-avoid-flood-in-the-city.ts
@@ -0,0 +1,28 @@
+function avoidFlood(rains: number[]): number[] {
+    const answer = rains.map(_ => -1)
+    const dryDays: number[] = []
+    const fullLakes: Record<number, number> = {}// lake -> day filled
+    for (let today = 0; today < rains.length; today++) {
+        const lake = rains[today]
+        if (lake === 0) {
+            dryDays.push(today)
+        } else {
+            const dayFilled = fullLakes[lake]
+            if (dayFilled !== undefined) {
+                const earliestDryDayIdx = dryDays.findIndex(dd => dd > dayFilled)
+                if (earliestDryDayIdx === -1) {
+                    return []
+                }
+                const earliestDryDay = dryDays[earliestDryDayIdx]
+                dryDays.splice(earliestDryDayIdx, 1)
+                answer[earliestDryDay] = lake
+            }
+
+            fullLakes[lake] = today
+        }
+    }
+    dryDays.forEach((day) => {
+        answer[day] = 1
+    })
+    return answer
+}
