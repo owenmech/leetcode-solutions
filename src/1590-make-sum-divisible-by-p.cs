cd83efff2e0c89a591e5948c843f74a57b0e1423
owenmech <31045445+owenmech@users.noreply.github.com>
2025-11-07 23:10:06 -0800

    1590

---
+++ src/1590-make-sum-divisible-by-p.cs
@@ -0,0 +1,48 @@
+public class Solution
+{
+	public int MinSubarray(int[] nums, int p)
+	{
+		int[] runningTotals = new int[nums.Length];
+		Dictionary<int, int> lastSeen = new(); // key -> most recent running total mod p equal to key
+		for (int i = 0; i < nums.Length; i++)
+		{
+			int total = (nums[i] % p + (i == 0 ? 0 : runningTotals[i - 1]) % p) % p;
+			runningTotals[i] = total;
+		}
+
+		int toRemove = runningTotals[^1];
+		if (toRemove == 0) return 0;
+		int shortest = int.MaxValue;
+
+		// find the shortest subarray whose sum is toRemove (mod p)
+		for (int i = 0; i < nums.Length; i++)
+		{
+			if (runningTotals[i] == toRemove)
+			{
+				shortest = Math.Min(shortest, i + 1);
+			}
+
+			// [0, i] - [0, j] = (j, i]
+			// the most recent [0, j] that makes the difference toRemove will give us the shortest
+			int lookingFor = (runningTotals[i] - toRemove + p) % p;
+			if (lastSeen.TryGetValue(lookingFor, out int j))
+			{
+				shortest = Math.Min(shortest, i - j);
+			}
+
+
+			lastSeen[runningTotals[i]] = i;
+		}
+
+		if (shortest >= nums.Length) return -1;
+		return shortest;
+	}
+}
+
+
+/* 
+ *
+ * 6  3  5  2
+ * 6  9  14 16 
+ *
+ */
